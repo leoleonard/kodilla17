0b1934014119dc4d4f40e4d025c9791f7a45894d
Damian Juzyk <damian.juzyk@gmail.com>
2018-01-25 04:11:43 +0400

    17.3

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
 var express = require('express');
+var bodyParser = require("body-parser");
+var fs = require("fs");
 var app = express();
+var stringifyFile;
 
-app.get("/", function(req, res) {
-  console.log("Żądanie GET!");
-  res.send("Hello GET!");
+app.use(bodyParser.json());
+
+ app.get("/getNote",  function (req, res) {
+   console.log("żądanie GET");
+
+   fs.readFile('./test.json', 'utf8', function(err, data) {
+       if (err) throw err;
+       stringifyFile = data
+       res.send(data);
+   });
+ });
+
+
+app.post("/updateNote/:note",  function (req, res) {
+  console.log("żądanie POST");
+  stringifyFile = req.params.note;
+
+  fs.writeFile("./test.json", stringifyFile, function (err) {
+    if (err) throw err;
+    console.log("file updated");
+  });
 });
 
-app.get("/list_user", function (req, res) {
-  console.log("żądanie GET do /list_user");
-  res.send("Strona w listą użytkowników");
+app.use(function (req, res, next) {
+  console.log("niewykryte działanie użytkownika");
+  res.status(404).send("Błąd ze strony użytkownika");
 });
 
-app.get("/ab*cd", function (req, res) {
-  console.log("żądanie GET do /ab*cd");
-  res.send("Wzór pasuje");
-});
-
-app.post("/", function (req, res) {
-  console.log("Żądanie POST");
-  res.send("Hello POST!");
-});
-
-app.delete("/del_user", function (req, res) {
-  console.log("Żądanie DELETE");
-  res.send("Hello DELETE!");
-});
-
-
-var server = app.listen(3000, function() {
-  console.log("Nasłuch na 3000");
-});
+app.listen(3000);
