bd06913b2ccd508e6535d6a0825f68d2573472fd
Damian Juzyk <damian.juzyk@gmail.com>
2018-01-25 17:52:43 +0400

    zadanie 17.4

--- server.js
+++ server.js
@@ -1,35 +1,24 @@
 var express = require('express');
-var bodyParser = require("body-parser");
-var fs = require("fs");
 var app = express();
 var stringifyFile;
 
-app.use(bodyParser.json());
+app.use(express.static('assets'));
 
- app.get("/getNote",  function (req, res) {
-   console.log("żądanie GET");
-
-   fs.readFile('./test.json', 'utf8', function(err, data) {
-       if (err) throw err;
-       stringifyFile = data
-       res.send(data);
-   });
- });
-
-
-app.post("/updateNote/:note",  function (req, res) {
-  console.log("żądanie POST");
-  stringifyFile = req.params.note;
-
-  fs.writeFile("./test.json", stringifyFile, function (err) {
-    if (err) throw err;
-    console.log("file updated");
-  });
+app.get('/', function (req, res) {
+    res.sendFile("/index.html")
 });
 
-app.use(function (req, res, next) {
-  console.log("niewykryte działanie użytkownika");
-  res.status(404).send("Błąd ze strony użytkownika");
+app.get('/userform', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end(JSON.stringify(response));
 });
 
-app.listen(3000);
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Nasłuch na http://' + host + ':' + port);
+});
