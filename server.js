a4376745dad4014ac45e92846fb75630d3fb4270
Damian Juzyk <damian.juzyk@gmail.com>
2018-01-26 21:53:48 +0400

    add zadanie 17.5

--- server.js
+++ server.js
@@ -4,21 +4,17 @@ var stringifyFile;
 
 app.use(express.static('assets'));
 
-app.get('/', function (req, res) {
-    res.sendFile("/index.html")
+app.get("/store", function (req, res, next) {
+  console.log("Jestem pośrednikiem przy żądaniu do /store");
+  next();
+})
+
+app.get("/store", function (req, res) {
+  res.send("To jest sklep");
 });
 
-app.get('/userform', function (req, res) {
-    const response = {
-        first_name: req.query.first_name,
-        last_name: req.query.last_name
-    };
-    res.end(JSON.stringify(response));
-});
+app.listen(3000);
 
-var server = app.listen(3000, 'localhost', function() {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Nasłuch na http://' + host + ':' + port);
+app.use(function (eq, res, next) {
+  res.status(404).send("Błąd użytkownika");
 });
