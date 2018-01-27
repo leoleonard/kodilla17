0c105df070dded429d14d62bb22f33affca5e3bb
Damian Juzyk <damian.juzyk@gmail.com>
2018-01-27 06:19:40 +0400

    add zadanie 17.6

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 var express = require('express');
 var app = express();
-var stringifyFile;
 
-app.use(express.static('assets'));
 
-app.get("/store", function (req, res, next) {
-  console.log("Jestem pośrednikiem przy żądaniu do /store");
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.use("/store", function (req, res, next) {
+  console.log("Pośrednik przy żądaniu do /store");
   next();
 })
 
+app.get("/", function (req, res) {
+  res.send("Hello world!");
+});
 app.get("/store", function (req, res) {
   res.send("To jest sklep");
+});
+
+app.get('/content', function(req, res){
+    res.render('content', {
+      url: "http://www.google.com"
+    });
+      });
+
+app.get("/first-template", function (req, res) {
+  res.render("first-template");
 });
 
 app.listen(3000);
