d69ead4cd00210f75114c756a20e56938593108c
Damian Juzyk <damian.juzyk@gmail.com>
2018-01-28 01:08:12 -0500

    add express jade

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 
-
 app.set("view engine", "pug");
 app.set("views", "./views");
 
@@ -22,6 +21,7 @@ app.get('/content', function(req, res){
       url: "http://www.google.com"
     });
       });
+
 
 app.get("/first-template", function (req, res) {
   res.render("first-template");
