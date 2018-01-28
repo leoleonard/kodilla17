ea0f0f40114e3ce7d38c1063a1b5347062737584
Damian Juzyk <damian.juzyk@gmail.com>
2018-01-28 00:26:37 -0500

    remove previous tasks

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "GOOGLE_CLIENT_ID": "111678941485-i344cpsobhbdpqv9n57ueb3d1930ebnh.apps.googleusercontent.com",
+  "GOOGLE_CLIENT_SECRET": "ilaNK5vnvIdGxULsulaQ-NNh",
+  "CALLBACK_URL": "http://localhost:3000/auth/google/callback/"
+};
